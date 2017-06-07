6cf9af0b292c556e0102af5acac725d3763bb5df
Hank Anderson <hank.p.anderson@gmail.com>
2017-06-06 21:41:36 -0500

    Generated gh-pages for commit 63fe7d6 Author: Hank Anderson <hank.p.anderson@gmail.com>
        Updated Jupyter notebook with output.

--- start-jupyter.sh
+++
@@ -1,3 +0,0 @@
-export DEXPY_ENV_NAME=coffee_talk
-source activate $DEXPY_ENV_NAME
-jupyter notebook --notebook-dir=. --no-browser --ip=0.0.0.0
--- to_slides.sh
+++
@@ -1,5 +0,0 @@
-export DEXPY_ENV_NAME=coffee_talk
-source activate $DEXPY_ENV_NAME
-jupyter nbconvert --to notebook dexpy-demo.ipynb --ClearOutputPreprocessor.enabled=True --ExecutePreprocessor.enabled=True
-jupyter nbconvert --to slides dexpy-demo.nbconvert.ipynb --template output_toggle --reveal-prefix=reveal.js
-
